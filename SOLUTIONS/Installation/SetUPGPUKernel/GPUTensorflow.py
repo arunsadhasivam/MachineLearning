1d09f24801baa7ef33d8e50152577e52a8ab2f1e
arun <arunsadhasivam@yahoo.co.in>
2025-02-16 01:07:05 -0800

    Create GPUTensorflow.py

---
+++ SOLUTIONS/Installation/SetUPGPUKernel/GPUTensorflow.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+    print("Please install GPU version of TF")
+tf.debugging.set_log_device_placement(True)
+
+# Create some tensors
+a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+c = tf.matmul(a, b)
+
+print(c)
+
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+with tf.Session() as sess:
+    print (sess.run(c))
